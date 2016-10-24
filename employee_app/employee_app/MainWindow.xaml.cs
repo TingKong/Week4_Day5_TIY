490d20067fb7dfb2481650beb40ea570cc4c2df5
Ting <Ting@DESKTOP-MJSRLVH>
2016-10-23 23:04:47 -0400

    Writing to a file

--- employee_app/employee_app/MainWindow.xaml.cs
+++ employee_app/employee_app/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.IO;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+
 
 namespace employee_app
 {
@@ -26,12 +28,19 @@ namespace employee_app
         {
             InitializeComponent();
             emp_list.ItemsSource = users ;
+          
         }
 
-       
-    
+        static void WriteFile(string file, List<Emp_detail> cycle)
+        {
+            
 
-    private void button_Click(object sender, RoutedEventArgs e)
+
+            
+        }
+
+
+        private void button_Click(object sender, RoutedEventArgs e)
         {
             Emp_detail newUser = new Emp_detail();
             newUser.UserName= textBox_name.Text;
@@ -42,15 +51,32 @@ namespace employee_app
             string emp_zip = textBox_zip.Text;
             newUser.UserZip = Convert.ToInt32(emp_zip);
 
+            newUser.UserHireDate = textBox_hireddate.Text;
+            newUser.UserTerm = textBox_termdate.Text;
 
 
             users.Add(newUser);
             emp_list.ItemsSource = null;
             emp_list.ItemsSource = users;
 
+           
+            
+
         }
 
+        private void button_Click_save(object sender, RoutedEventArgs e)
+        {
+            string fileName = "C:\\Users\\Ting\\Source\\Repos\\Week4_Day5_TIY\\employee_app\\employee_list.txt";
 
+            using (StreamWriter sw = new StreamWriter(fileName, true))
+            {
+                sw.WriteLine(textBox_id.Text + ", " + textBox_name.Text + ", " + textBox_add.Text + ", " + textBox_zip.Text + " , " +  ", " + textBox_termdate.Text);
 
+            }
+
+        
+
+           
+        }
     }
 }
